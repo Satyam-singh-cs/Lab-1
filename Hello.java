2331d963a7a8fc06b4568761dd5c3e0337cd6d20
Satyam-singh-cs <89561870+Satyam-singh-cs@users.noreply.github.com>
2021-08-26 00:11:47 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,24 @@
+Last login: Wed Aug 25 23:15:15 on ttys000
+satyamsingh@Satyams-MacBook-Air ~ % cd downloads
+satyamsingh@Satyams-MacBook-Air downloads % javac Hello.java
+satyamsingh@Satyams-MacBook-Air downloads % java Hello      
+<Satyam Singh>
+<Hello.java>
+<sublime text/Lab 1>
+<August 24th>
+<CMSC255 and Section 1>
+
+
+ ________________________________________________________________________
+   _     _       ______                                                 
+  |     / \        ||                                                  
+   l    ----       ||   \   /  //\\     //\\    //\\             
+  _/   /   \       ||    \ /   ----    //  \\  //  \\             
+                          /   //  \\  //    \\//    \\           
+           ^             /                                              
+       ____|____                      ______                            
+      (         )                    (      )                           
+     (           )       _          (        )        _                 
+    (             )     ( )        (          )      ( )                
+___(_______________)___(___)______(____________)____(___)_______________
+satyamsingh@Satyams-MacBook-Air downloads % 
